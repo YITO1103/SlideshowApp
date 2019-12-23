e81d364dfef17d3afe9be239af0a0b8a1f03edde
YITO1103 <yuta.j.1103@gmail.com>
2019-12-23 17:47:36 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by user1 on 2019/12/23.
+//  Copyright © 2019 yutaka.ito4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
