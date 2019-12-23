263ecb0600de76b716a19ce2d2db8014da946bff
YITO1103 <yuta.j.1103@gmail.com>
2019-12-23 18:53:06 +0900

    add img

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //for deb
+        print(NSHomeDirectory())
+        
+        // ファイル一覧を取得するためfileManagerをインスタンス
+        let fileManager = FileManager()
+         
+        // ファイル一覧の場所であるpathを文字列で取得
+        //let path = Bundle.main.bundlePath
+        let path = Bundle.main.bundlePath + "/images"
+
+        do {
+            let files = try fileManager.contentsOfDirectory(atPath: path)
+         
+            // png画像だけを集める配列を用意
+            var images : [String] = []
+            
+            for file in files {
+                // ファイル名の後方が.pngであればtrueとなる
+                if file.hasSuffix(".png") {
+                    images.append(file)
+                }
+            }
+            print("----------------------")
+            print( images)
+            
+        }
+        catch let error {
+            print("----------------------")
+
+            print( error)
+        }
     }
-
-
 }
 
