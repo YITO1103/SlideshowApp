17c8fabef434061f56dfb9d9275686290342d550
YITO1103 <yuta.j.1103@gmail.com>
2019-12-24 19:00:57 +0900

    一枚目の画面３０%

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum CMD: String {
+        case c_next = "次"
+        case c_prev = "前"
+        case c_init = "はじめ"
+    }    //　指定
+    func showImage(_ sCmd : CMD) {
+        
+        switch sCmd {
+        case CMD.c_next:
+            iCurrentIndex -= 1
+            if iCurrentIndex < 0  {
+                iCurrentIndex = arrPathImageFile.count - 1
+            }
+            break
+        case CMD.c_prev:
+            iCurrentIndex += 1
+            if iCurrentIndex >= arrPathImageFile.count {
+                iCurrentIndex = 0
+            }
+            break
+        default:
+            iCurrentIndex = 0
+            break
+        }
+        print("prev iCurrentIndex>>" + iCurrentIndex.description + ":::" + arrPathImageFile[iCurrentIndex])
+        print("----------------------")
+        print( arrPathImageFile)
+        print("----------------------")
+        print( arrPathImageFile.count)
+        print("prev iCurrentIndex>>" + iCurrentIndex.description + ":::" + arrPathImageFile[iCurrentIndex])
+        // 画像ファイルを読み込み Image Viewに画像を設定
+         
+        imageView.image = nil
+        imageView.setNeedsDisplay()
+        imageView.image = UIImage(named: arrPathImageFile[iCurrentIndex])
+     }
+
+    //表示対象の画像のパスの配列
+    var arrPathImageFile : [String] = []
+    // 表示中のアイテムNo.
+    var iCurrentIndex:Int=0
+
+    // 進むをタップ
+    @IBAction func btnNext(_ sender: Any) {
+        showImage(CMD.c_next)
+    }
+    // 戻るをタップ
+    @IBAction func btnPrev(_ sender: Any) {
+        showImage(CMD.c_prev)
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var sImgDirPath:String=""
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
+
         //for deb
         print(NSHomeDirectory())
         
@@ -23,29 +76,50 @@ class ViewController: UIViewController {
          
         // ファイル一覧の場所であるpathを文字列で取得
         //let path = Bundle.main.bundlePath
-        let path = Bundle.main.bundlePath + "/images"
+        sImgDirPath = Bundle.main.bundlePath + "/images"
 
         do {
-            let files = try fileManager.contentsOfDirectory(atPath: path)
+            let files = try fileManager.contentsOfDirectory(atPath: sImgDirPath)
          
             // png画像だけを集める配列を用意
-            var images : [String] = []
+            // todo: 画像形式。画像数
             
             for file in files {
                 // ファイル名の後方が.pngであればtrueとなる
                 if file.hasSuffix(".png") {
-                    images.append(file)
+                    let sPath = sImgDirPath + "/" + file
+                    arrPathImageFile.append(sPath)
+                    //print(sPath)
                 }
             }
+
+            /*
             print("----------------------")
-            print( images)
-            
+             print( arrPathImageFile)
+            print("----------------------")
+            print( arrPathImageFile.count)
+            print("prev iCurrentIndex>>" + iCurrentIndex.description + ":::" + arrPathImageFile[iCurrentIndex])
+            // 画像ファイルを読み込み Image Viewに画像を設定
+            imageView.image = nil
+            imageView.setNeedsDisplay()
+            imageView.image = UIImage(named: arrPathImageFile[iCurrentIndex])
+            */
         }
         catch let error {
             print("----------------------")
 
             print( error)
         }
+        showImage(CMD.c_init)
+
+        
+        /*
+        let sImgPath = sImgDirPath + "/01.png"       // バンドルした画像ファイルを読み込み
+        let image = UIImage(named: sImgPath)
+        // Image Viewに画像を設定
+        imageView.image = image
+         */
+        
     }
 }
 
